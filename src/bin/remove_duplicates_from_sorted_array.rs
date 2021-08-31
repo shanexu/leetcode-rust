c886c894b4f59494845e54a0fdc0b32b33dccabb
shanexu <xusheng0711@gmail.com>
2021-08-31 10:14:16 +0800

    add problem

---
+++ src/bin/remove_duplicates_from_sorted_array.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("{}", Solution::remove_duplicates(&mut vec![1, 1, 2]));
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        // TODO
+        0
+    }
+}
