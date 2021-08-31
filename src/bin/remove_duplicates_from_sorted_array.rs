9c29d72df6e4c42871607c2fc8ce8a75d195436e
shanexu <xusheng0711@gmail.com>
2021-08-31 18:27:15 +0800

    solved remove_duplicates_from_sorted_array

--- src/bin/remove_duplicates_from_sorted_array.rs
+++ src/bin/remove_duplicates_from_sorted_array.rs
@@ -1,12 +1,34 @@
 fn main() {
-    println!("{}", Solution::remove_duplicates(&mut vec![1, 1, 2]));
+    let mut nums = vec![];
+    let n = Solution::remove_duplicates(&mut nums);
+    printvec(&nums, n);
 }
 
 struct Solution {}
 
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        // TODO
-        0
+        if nums.len() == 0 {
+            return 0
+        }
+        let mut p = nums[0];
+        let mut idx: usize = 0;
+        for i in 1..nums.len() {
+            let x = nums[i];
+            if p != x {
+                p = x;
+                idx+=1;
+                if idx != i {
+                    nums[idx] = x;
+                }
+            }
+        }
+        (idx+1) as i32
+    }
+}
+
+fn printvec(nums: &Vec<i32>, n: i32) {
+    for i in 0..(n as usize) {
+        println!("{}", nums[i])
     }
 }
