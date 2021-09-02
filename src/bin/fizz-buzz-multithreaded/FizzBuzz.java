714cd4bed45d45c7d2e6cf57dcd6c02774ba1acc
shanexu <xusheng0711@gmail.com>
2021-09-02 16:36:34 +0800

    try FizzBuzz

---
+++ src/bin/fizz-buzz-multithreaded/FizzBuzz.java
@@ -0,0 +1,75 @@
+import java.util.function.IntConsumer;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+    }
+
+    private int n;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    private volatile int i = 1;
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        while (i <= n) {
+            synchronized(this) {
+                if (i % 3 == 0 && i % 5 != 0) {
+                    printFizz.run();
+                    i++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        while (i <= n) {
+            synchronized(this) {
+                if (i % 5 == 0 && i % 3 != 0) {
+                    printBuzz.run();
+                    i++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (i <= n) {
+            synchronized(this) {
+                if (i % 15 == 0) {
+                    printFizzBuzz.run();
+                    i++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        while (i <= n) {
+            synchronized(this) {
+                if (i % 3 != 0 && i % 5 != 0) {
+                    printNumber.accept(i);
+                    i++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+}
