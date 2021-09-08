bf07cf4ffb66fd76ffd3848eaf669ec9b7199cb6
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-09-08 13:26:02 +0800

    solved remove-element

---
+++ src/bin/remove-element.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut nums = vec![1, 3, 3];
+    let n = Solution::remove_element(&mut nums, 3);
+    println!("n={}", n);
+    printvec(&nums, n);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+        let mut idx: usize = 0;
+        for i in 0..nums.len() {
+            let x = nums[i];
+            if x != val {
+                if idx != i {
+                    nums[idx] = x;
+                }
+                idx += 1;
+            }
+        }
+        idx as i32
+    }
+}
+
+//
+fn printvec(nums: &Vec<i32>, n: i32) {
+    for i in 0..(n as usize) {
+        println!("{}", nums[i])
+    }
+}
