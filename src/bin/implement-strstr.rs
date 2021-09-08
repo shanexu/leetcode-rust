df59725a2bd24258e9a0e36a7b5037af456ab710
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-09-08 14:15:13 +0800

    fix warning

--- src/bin/implement-strstr.rs
+++ src/bin/implement-strstr.rs
@@ -1,10 +1,17 @@
+fn main() {
+    println!(
+        "{}",
+        Solution::str_str(String::from("helloworld"), String::from("hello"))
+    );
+}
+
 struct Solution {}
 
 impl Solution {
     pub fn str_str(haystack: String, needle: String) -> i32 {
-        match haystack.find(&needle, None) {
+        match haystack.find(&needle) {
             Some(i) => i as i32,
-            None => -1
+            None => -1,
         }
     }
 }
--- src/bin/trapping-rain-water.rs
+++ src/bin/trapping-rain-water.rs
@@ -1,32 +1,40 @@
+fn main() {
+    println!(
+        "{}",
+        Solution::trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+    );
+    println!("{}", Solution::trap(vec![4, 2, 0, 3, 2, 5]));
+}
+
 struct Solution {}
 
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
         let n = height.len();
         let mut left = vec![0; n];
-        for i in (0..n) {
+        for i in 0..n {
             if i == 0 {
                 left[i] = height[i];
                 continue;
             }
-            left[i] = max(left[i-1],height[i]);
+            left[i] = max(left[i - 1], height[i]);
         }
         let mut right = vec![0; n];
         for i in (0..n).rev() {
-            if i == n-1 {
+            if i == n - 1 {
                 right[i] = height[i];
                 continue;
             }
-            right[i] = max(right[i+1], height[i]);
+            right[i] = max(right[i + 1], height[i]);
         }
         let mut s = 0;
-        for i in (0..n) {
+        for i in 0..n {
             let h = height[i];
-		    let l = left[i];
-		    let r = right[i];
-		    if h < l && h < r {
-			    s += min(l, r) - h;
-		    }
+            let l = left[i];
+            let r = right[i];
+            if h < l && h < r {
+                s += min(l, r) - h;
+            }
         }
         s
     }
