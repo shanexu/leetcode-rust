548a000dc8cea3776759f35ec8a0a165fa84b3f2
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-09-12 23:59:31 +0800

    solved rotate-image

---
+++ src/bin/rotate-image.rs
@@ -0,0 +1,35 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let n = matrix.len();
+        for i in 0..matrix.len() {
+            for j in 0..matrix[i].len() {
+                matrix[j][n - i - 1] =
+                    combine_old_new_value(matrix[j][n - i - 1], get_old_value(matrix[i][j]));
+            }
+        }
+        for i in 0..matrix.len() {
+            for j in 0..matrix[i].len() {
+                matrix[i][j] = get_new_value(matrix[i][j]);
+            }
+        }
+    }
+}
+
+#[inline]
+fn get_old_value(x: i32) -> i32 {
+    ((x as u32) & 0x0000ffff) as i16 as i32
+}
+
+#[inline]
+fn combine_old_new_value(x: i32, y: i32) -> i32 {
+    (((y as u32 & 0x0000ffff) << 16) | (x as u32 & 0x0000ffff)) as i32
+}
+
+#[inline]
+fn get_new_value(x: i32) -> i32 {
+    ((x as u32) >> 16) as i16 as i32
+}
