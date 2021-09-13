987adf37f42ff3634f432c17bf100b31cf5d3939
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-09-13 12:05:30 +0800

    solved merge-two-sorted-lists

---
+++ src/bin/merge-two-sorted-lists.rs
@@ -0,0 +1,52 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut fake_head = ListNode::new(0);
+        let mut cursor = &mut fake_head;
+        let mut cursor1 = &l1;
+        let mut cursor2 = &l2;
+        loop {
+            match (cursor1, cursor2) {
+                (Some(v1), Some(v2)) => {
+                    if v1.val < v2.val {
+                        cursor.next = Some(Box::new(ListNode::new(v1.val)));
+                        cursor = cursor.next.as_mut().unwrap();
+                        cursor1 = &v1.next;
+                    } else {
+                        cursor.next = Some(Box::new(ListNode::new(v2.val)));
+                        cursor = cursor.next.as_mut().unwrap();
+                        cursor2 = &v2.next;
+                    }
+                }
+                (Some(v1), None) => {
+                    cursor.next = Some(Box::new(ListNode::new(v1.val)));
+                    cursor = cursor.next.as_mut().unwrap();
+                    cursor1 = &v1.next;
+                }
+                (None, Some(v2)) => {
+                    cursor.next = Some(Box::new(ListNode::new(v2.val)));
+                    cursor = cursor.next.as_mut().unwrap();
+                    cursor2 = &v2.next;
+                }
+                (None, None) => break,
+            }
+        }
+        fake_head.next
+    }
+}
