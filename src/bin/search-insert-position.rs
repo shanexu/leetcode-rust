93f1425101630f4bafad2ca72a7e03f19d57cfe9
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-09-13 23:24:25 +0800

    solved search-insert-position

---
+++ src/bin/search-insert-position.rs
@@ -0,0 +1,12 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        match nums.binary_search(&target) {
+            Ok(i) => i as i32,
+            Err(i) => i as i32,
+        }
+    }
+}
