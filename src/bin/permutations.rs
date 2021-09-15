e61f7fa90a1f18d1183d706ecfa7224d7bc65675
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-09-15 21:09:18 +0800

    solved permutations

--- src/bin/permutations.rs
+++ src/bin/permutations.rs
@@ -1,27 +1,34 @@
 fn main() {
-    println!("{:?}", Solution::permute(vec![1,2,3,4]))
+    println!("{:?}", Solution::permute(vec![1, 2, 3, 4]))
 }
 
 struct Solution;
 
 impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let step = nums.len() - 1;
-        let mut results = vec![];
+        let mut results = Vec::with_capacity(size(nums.len()));
         let mut nums = nums;
-        solve(&mut results, &mut nums, step);
+        solve(&mut results, &mut nums, 0);
         results
     }
 }
 
-fn solve(results: &mut Vec<Vec<i32>>, nums: &mut Vec<i32>, step: usize) {
-    if step == 0 {
-        results.push(nums.clone());
-        return
+fn size(n: usize) -> usize {
+    let mut p = 1;
+    for i in 1..n+1 {
+        p = p*i;
     }
-    for i in (0..(step+1)).rev() {
+    p
+}
+
+fn solve(results: &mut Vec<Vec<i32>>, nums: &mut Vec<i32>, step: usize) {
+    if step == nums.len() - 1 {
+        results.push(nums.clone());
+        return;
+    }
+    for i in step..nums.len() {
         nums.swap(step, i);
-        solve(results, nums, step - 1);
+        solve(results, nums, step + 1);
         nums.swap(step, i);
     }
 }
