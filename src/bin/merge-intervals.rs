2d24680bb6aa1b4d8b573695ed346800a2cfb5f4
Shane Xu <shane@Shanes-MacBook-Pro.local>
2021-10-07 12:38:48 +0800

    improve merge-intervals

--- src/bin/merge-intervals.rs
+++ src/bin/merge-intervals.rs
@@ -1,13 +1,17 @@
 fn main() {
     println!(
         "{:?}",
+        Solution1::merge(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]])
+    );
+    println!(
+        "{:?}",
         Solution::merge(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]])
     );
 }
 
-struct Solution;
+struct Solution1;
 
-impl Solution {
+impl Solution1 {
     pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let mut intervals = intervals;
         let mut results = vec![];
@@ -108,4 +112,44 @@ fn binary_heap_left(i: usize) -> usize {
 
 fn binary_heap_right(i: usize) -> usize {
     2 * i + 2
+}
+
+struct Solution;
+
+use std::cmp::Ordering;
+impl Solution {
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut intervals = intervals;
+        let n = intervals.len();
+        intervals.sort_by(|x, y| match x[0].cmp(&y[0]) {
+            Ordering::Equal => x[1].cmp(&y[1]),
+            others => others,
+        });
+        let mut results = vec![];
+        let mut k = 0;
+        for i in 1..intervals.len() {
+            let x0 = intervals[i - 1][0];
+            let x1 = intervals[i - 1][1];
+            let y0 = intervals[i][0];
+            let y1 = intervals[i][1];
+            if x1 < y0 {
+                results.push(vec![x0, x1]);
+                intervals[k][0] = x0;
+                intervals[k][1] = x1;
+                k += 1;
+            } else {
+                intervals[i] = vec![x0, if x1 > y1 { x1 } else { y1 }];
+            }
+        }
+        if k != n - 1 {
+            let x0 = intervals[n - 1][0];
+            let x1 = intervals[n - 1][1];
+            intervals[k][0] = x0;
+            intervals[k][1] = x1;
+            unsafe {
+                intervals.set_len(k + 1);
+            }
+        }
+        intervals
+    }
 }
