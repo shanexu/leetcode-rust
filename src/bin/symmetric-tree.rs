3216f5b1b18c75cf9f5fdbc9f205e4dbd47aaa07
Shane Xu <xusheng0711@gmail.com>
2024-12-14 10:29:48 +0800

    add solution

---
+++ src/bin/symmetric-tree.rs
@@ -0,0 +1,37 @@
+use leetcode_rust::tree_node::TreeNode;
+
+fn main() {
+
+}
+
+
+struct Solution;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        fn helper(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+            match (p, q) {
+                (None, None) => true,
+                (Some(_), None) | (None, Some(_)) => false,
+                (Some(p), Some(q)) => {
+                    let p = p.borrow();
+                    let q = q.borrow();
+                    if p.val != q.val {
+                        false
+                    } else {
+                        helper(p.left.clone(), q.right.clone()) && helper(p.right.clone(), q.left.clone())
+                    }
+                }
+            }
+        }
+
+        if let Some(root) = root {
+            helper(root.borrow().left.clone(), root.borrow().right.clone())
+        } else {
+            true
+        }
+    }
+}
