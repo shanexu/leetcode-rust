5128001bbde6e0521a2c25bc6534cceb830bdf52
Shane Xu <xusheng0711@gmail.com>
2024-12-18 13:28:43 +0800

    add solution

--- src/bin/final-prices-with-a-special-discount-in-a-shop.rs
+++ src/bin/final-prices-with-a-special-discount-in-a-shop.rs
@@ -1,9 +1,12 @@
+use std::os::linux::raw::stat;
+
 fn main() {
 
 }
 
 struct Solution;
 
+/// 暴力算法
 impl Solution {
     pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
         let mut final_prices = prices.clone();
@@ -15,6 +18,23 @@ impl Solution {
                     break;
                 }
             }
+        }
+        final_prices
+    }
+}
+
+/// 使用stack
+struct Solution2;
+impl Solution2 {
+    pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
+        let mut final_prices = prices.clone();
+        let mut stack = vec![];
+        for i in 0..final_prices.len() {
+            while !stack.is_empty() && prices[stack[stack.len()-1]] >= prices[i] {
+                let p = stack.pop().unwrap();
+                final_prices[p] -= prices[i];
+            }
+            stack.push(i);
         }
         final_prices
     }
