785cff76e36909fb57f5bb2f284300813993039e
Shane Xu <xusheng0711@gmail.com>
2024-12-18 18:52:27 +0800

    add solution

--- src/bin/using-a-robot-to-print-the-lexicographically-smallest-string.rs
+++ src/bin/using-a-robot-to-print-the-lexicographically-smallest-string.rs
@@ -1,41 +1,28 @@
 fn main() {
-    println!("{}", Solution::robot_with_string(String::from("bydizfve")));
+    println!("{}", Solution::robot_with_string(String::from("zza")));
 }
 
 struct Solution;
 
-use std::collections::BTreeSet;
 impl Solution {
     pub fn robot_with_string(s: String) -> String {
         let bs = s.as_bytes();
-        let n = bs.len();
-        let mut tree = BTreeSet::new();
-        for i in 0..bs.len() {
-            tree.insert((bs[i], i));
+        let mut stack = vec![];
+        let mut result = vec![];
+        let mut counter = vec![0; 26];
+        for &c in bs {
+            counter[(c - b'a') as usize] += 1;
         }
-        let mut i = 0;
-        let mut stack: Vec<u8> = vec![];
-        let mut result: Vec<u8> = vec![];
-        while result.len() < n {
-            if tree.is_empty() {
-                while let Some(c) = stack.pop() {
-                    result.push(c);
-                }
-                continue;
+        let mut min_char = b'a';
+        for &c in bs {
+            counter[(c - b'a') as usize] -= 1;
+            stack.push(c);
+            while min_char < b'z' && counter[(min_char - b'a') as usize] == 0 {
+                min_char += 1;
             }
-            if !stack.is_empty() && stack[stack.len() - 1] <= tree.first().unwrap().0 {
-                let c = stack.pop().unwrap();
-                result.push(c);
-                continue;
+            while !stack.is_empty() && stack[stack.len() - 1] <= min_char {
+                result.push(stack.pop().unwrap());
             }
-            let (c, j) = tree.pop_first().unwrap();
-            result.push(c);
-            for k in i..j {
-                let v = bs[k];
-                stack.push(v);
-                tree.remove(&(v, k));
-            }
-            i = j + 1;
         }
         String::from_utf8(result).unwrap()
     }
