0f3c8a8dfd5173b8d99ca69722d5d6cdbe1cbe33
Shane Xu <xusheng0711@gmail.com>
2024-12-21 19:52:39 +0800

    add solution

--- src/bin/car-fleet.rs
+++ src/bin/car-fleet.rs
@@ -21,3 +21,31 @@ impl Solution {
         stack.len() as i32
     }
 }
+
+/// 用了桶排序
+struct Solution2;
+
+impl Solution2 {
+    pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {
+        let mut ts = vec![0.0; target as usize];
+        let mut stack = vec![];
+        let n = position.len();
+        for i in 0..n {
+            let p = position[i];
+            let d = target - p;
+            let v = speed[i];
+            let t = d as f64 / v as f64;
+            ts[p as usize] = t;
+        }
+        for t in ts {
+            if t == 0.0 {
+                continue;
+            }
+            while !stack.is_empty() && t >= stack[stack.len() - 1] {
+                stack.pop();
+            }
+            stack.push(t);
+        }
+        stack.len() as i32
+    }
+}
