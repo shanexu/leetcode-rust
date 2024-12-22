9ce14e09511ccc1eff9e36cf406a6152a999cafb
Shane Xu <xusheng0711@gmail.com>
2024-12-22 15:04:14 +0800

    add solution

---
+++ src/bin/beautiful-towers-i.rs
@@ -0,0 +1,49 @@
+fn main() {
+    // println!("{}", Solution::maximum_sum_of_heights(vec![2, 4, 6, 4, 1]));
+    println!("{}", Solution::maximum_sum_of_heights(vec![5, 3, 4, 1, 1]));
+}
+
+/// 这个用了类似 largest-rectangle-in-histogram 的方法来线性求和
+struct Solution;
+
+impl Solution {
+    pub fn maximum_sum_of_heights(mut heights: Vec<i32>) -> i64 {
+        fn helper(heights: &Vec<i32>) -> Vec<i64> {
+            let mut stack: Vec<usize> = vec![];
+            let mut s: i64 = 0;
+            let mut ss = vec![];
+            for (i, &v) in heights.iter().enumerate() {
+                while !stack.is_empty() && heights[stack[stack.len() - 1]] > v {
+                    let j = stack.pop().unwrap();
+                    let width = if stack.is_empty() {
+                        j + 1
+                    } else {
+                        j - stack[stack.len() - 1]
+                    };
+                    s -= (width as i64) * (heights[j] as i64);
+                }
+                let width = if stack.is_empty() {
+                    i + 1
+                } else {
+                    i - stack[stack.len() - 1]
+                };
+                s += (width as i64) * (v as i64);
+                ss.push(s);
+                stack.push(i);
+            }
+            ss
+        }
+        let n = heights.len();
+        // 从左往右算一遍
+        let s1 = helper(&heights);
+        heights.reverse();
+        // 从右往左算一遍
+        let s2 = helper(&heights);
+        let mut m = i64::MIN;
+        for i in 0..n {
+            let s = s1[i] + s2[n - i - 1] - heights[n - i - 1] as i64;
+            m = std::cmp::max(m, s);
+        }
+        m
+    }
+}
