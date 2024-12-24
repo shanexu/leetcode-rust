03e44ab2aad66ceea973d00fdf105047ee2eff72
Shane Xu <xusheng0711@gmail.com>
2024-12-24 09:39:30 +0800

    add solution

---
+++ src/bin/minimum-cost-tree-from-leaf-values.rs
@@ -0,0 +1,24 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn mct_from_leaf_values(arr: Vec<i32>) -> i32 {
+        let mut stack = vec![i32::MAX];
+        let mut result = 0;
+
+        for &v in arr.iter() {
+            while stack[stack.len() - 1] <= v {
+                let u = stack.pop().unwrap();
+                result += u * std::cmp::min(stack[stack.len() - 1], v);
+            }
+            stack.push(v);
+        }
+
+        for i in 2..stack.len() {
+            result += stack[i] * stack[i - 1];
+        }
+
+        result
+    }
+}
