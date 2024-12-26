5166bfdd9927b2fc4760778e2fcffd98883b4761
Shane Xu <xusheng0711@gmail.com>
2024-12-26 13:35:51 +0800

    add solution

---
+++ src/bin/linked-list-cycle.c
@@ -0,0 +1,39 @@
+#include "list_node.h"
+#include <cstddef>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+  struct ListNode *fast = head;
+  struct ListNode *middle = head;
+  struct ListNode *slow = head;
+  if (head == NULL) {
+    return false;
+  }
+  while (true) {
+    fast = fast->next;
+    if (fast == NULL) {
+      return false;
+    }
+    fast = fast->next;
+    if (fast == NULL) {
+      return false;
+    }
+    fast = fast->next;
+    if (fast == NULL) {
+      return false;
+    }
+    middle = middle->next->next;
+    slow = slow->next;
+    if (fast == slow || fast == middle || middle == slow) {
+      return true;
+    }
+  }
+}
