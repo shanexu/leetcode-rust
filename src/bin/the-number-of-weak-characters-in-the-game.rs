3312fc6137ac42a578218c3a9542f3fbc9d46ffe
Shane Xu <xusheng0711@gmail.com>
2024-12-26 10:46:31 +0800

    add solution

--- src/bin/the-number-of-weak-characters-in-the-game.rs
+++ src/bin/the-number-of-weak-characters-in-the-game.rs
@@ -1,9 +1,11 @@
-use std::collections::VecDeque;
-
 fn main() {
     println!(
         "{}",
         Solution::number_of_weak_characters(vec![vec![1, 1], vec![2, 1], vec![2, 2], vec![1, 2]])
+    );
+    println!(
+        "{}",
+        Solution::number_of_weak_characters(vec![vec![10, 2], vec![10, 1]])
     );
 }
 
@@ -11,21 +13,16 @@ struct Solution;
 
 impl Solution {
     pub fn number_of_weak_characters(mut properties: Vec<Vec<i32>>) -> i32 {
-        properties.sort_by(|a, b| a[0].cmp(&b[0]).then(a[1].cmp(&b[1])));
+        properties.sort_by(|a, b| a[0].cmp(&b[0]).reverse().then(a[1].cmp(&b[1])));
         let mut count = 0;
-        let mut stacks: std::collections::BTreeMap<i32, std::collections::VecDeque<Vec<i32>>> =
-            std::collections::BTreeMap::new();
+        let mut max_defense = 0;
         for c in properties.iter() {
-            for (_, s) in stacks.range_mut(..c[0]) {
-                while !s.is_empty() && s[0][1] < c[1] {
-                    s.pop_front();
-                    count += 1;
-                }
+            if c[1] < max_defense {
+                count += 1;
             }
-            stacks
-                .entry(c[0])
-                .or_insert(std::collections::VecDeque::new())
-                .push_back(c.to_vec());
+            {
+                max_defense = c[1];
+            }
         }
         count
     }
