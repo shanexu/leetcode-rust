8dd16dce6e99fe6de880331eaf59235c9580cd48
Shane Xu <xusheng0711@gmail.com>
2024-12-28 00:27:44 +0800

    add solution

---
+++ src/bin/sum-of-squares-of-special-elements.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{}", 1 % 1);
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn sum_of_squares(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut s = 0;
+        for (i, &v) in nums.iter().enumerate() {
+            if n % (i + 1) == 0 {
+                s += v*v;
+            }
+        }
+        s
+    }
+}
