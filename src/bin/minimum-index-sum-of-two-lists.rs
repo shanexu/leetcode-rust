5e47ea6d74fcfe6cd241bd8fbe625c3f89cf6130
Shane Xu <xusheng0711@gmail.com>
2024-12-28 19:09:51 +0800

    refine

--- src/bin/minimum-index-sum-of-two-lists.rs
+++ src/bin/minimum-index-sum-of-two-lists.rs
@@ -10,19 +10,23 @@ impl Solution {
         for (i, &ref v) in list1.iter().enumerate() {
             map.insert(v, i);
         }
-        let mut ans = vec![];
+        let mut ans_idx = vec![];
         let mut min_index_sum = list1.len() + list2.len();
         for (j, v) in list2.iter().enumerate() {
             if let Some(&i) = map.get(v) {
                 if i + j < min_index_sum {
-                    ans.clear();
-                    ans.push(j);
+                    ans_idx.clear();
+                    ans_idx.push(j);
                     min_index_sum = i + j;
                 } else if i + j == min_index_sum {
-                    ans.push(j);
+                    ans_idx.push(j);
                 }
             }
         }
-        ans.iter().map(|&i| list2[i].clone()).collect()
+        let mut ans = Vec::with_capacity(ans_idx.len());
+        for i in ans_idx {
+            ans.push(list2[i].clone());
+        }
+        ans
     }
 }
