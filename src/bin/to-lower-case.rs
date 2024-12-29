1b6416bed1baf2d281dfdf01ea18687de2e9c5d2
Shane Xu <xusheng0711@gmail.com>
2024-12-29 17:27:40 +0800

    add solution

---
+++ src/bin/to-lower-case.rs
@@ -0,0 +1,15 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn to_lower_case(mut s: String) -> String {
+        let bs = unsafe { s.as_bytes_mut() };
+        for i in 0..bs.len() {
+            if bs[i].is_ascii_uppercase() {
+                bs[i] = bs[i].to_ascii_lowercase();
+            }
+        }
+        s
+    }
+}
