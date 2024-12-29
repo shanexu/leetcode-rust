ab674750eb9cd37ddda2e96091f847daae06af58
Shane Xu <xusheng0711@gmail.com>
2024-12-29 17:42:53 +0800

    add solution

---
+++ src/bin/robot-return-to-origin.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let mut h = 0;
+        let mut v = 0;
+        for &d in moves.as_bytes() {
+            match d {
+                b'L' => h -= 1,
+                b'R' => h += 1,
+                b'U' => v += 1,
+                b'D' => v -= 1,
+                _ => {},
+            }
+        }
+        h == 0 && v == 0
+    }
+}
