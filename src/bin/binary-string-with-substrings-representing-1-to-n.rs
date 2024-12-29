6c340eb58a3d748e669870854eb2799478d99c8c
Shane Xu <xusheng0711@gmail.com>
2024-12-29 19:11:52 +0800

    add solution

---
+++ src/bin/binary-string-with-substrings-representing-1-to-n.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("{}", Solution::query_string("1001".to_string(), 3));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn query_string(s: String, n: i32) -> bool {
+        for i in (n/2+1)..=n {
+            if !s.contains(format!("{:b}", i).as_str()) {
+                return false;
+            }
+        }
+        true
+    }
+}
