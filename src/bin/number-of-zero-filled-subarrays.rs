72a0d438c709c0cb3cce6b337d1482f2e487f8f2
Shane Xu <xusheng0711@gmail.com>
2024-12-30 21:06:38 +0800

    add solution

---
+++ src/bin/number-of-zero-filled-subarrays.rs
@@ -0,0 +1,33 @@
+fn main() {
+    println!(
+        "{}",
+        Solution::zero_filled_subarray(vec![0, 0, 0, 2, 0, 0])
+    );
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn zero_filled_subarray(nums: Vec<i32>) -> i64 {
+        let mut ans = 0;
+        let mut j_opt: Option<usize> = None;
+        for (i, &x) in nums.iter().enumerate() {
+            if x == 0 {
+                if j_opt.is_none() {
+                    j_opt = Some(i);
+                }
+            } else {
+                if let Some(j) = j_opt {
+                    let t = i - j;
+                    ans += ((t + 1) * t / 2) as i64;
+                    j_opt = None;
+                }
+            }
+        }
+        if let Some(j) = j_opt {
+            let t = nums.len() - j;
+            ans += ((t + 1) * t / 2) as i64;
+        }
+        ans
+    }
+}
