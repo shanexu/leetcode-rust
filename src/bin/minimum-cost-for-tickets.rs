822bc97c3bcccad18cf640d53ba44a1aa3a8f455
Shane Xu <xusheng0711@gmail.com>
2024-12-31 15:42:43 +0800

    refine

--- src/bin/minimum-cost-for-tickets.rs
+++ src/bin/minimum-cost-for-tickets.rs
@@ -23,23 +23,24 @@ impl Solution {
         ans[0] = c1.min(c7).min(c30);
         for i in 1..days.len() {
             let prev_day = days[i - 1] as usize;
-            let prev_day_cost = ans[prev_day - start_day];
+            let prev_day_idx = prev_day - start_day;
+            let prev_day_cost = ans[prev_day_idx];
             let current_day = days[i] as usize;
             let current_day_idx = current_day - start_day;
-            for j in (prev_day + 1 - start_day)..(current_day_idx) {
+            for j in (prev_day_idx + 1)..current_day_idx {
                 ans[j] = prev_day_cost;
             }
             let mut current_day_cost = ans[current_day_idx - 1] + c1;
-            if current_day < start_day + 7 {
+            if current_day_idx < 7 {
                 current_day_cost = current_day_cost.min(c7);
             }
-            for t in (if current_day < start_day + 7 { 0 } else { current_day_idx - 7 })..current_day_idx {
+            for t in (if current_day_idx <  7 { 0 } else { current_day_idx - 7 })..current_day_idx {
                 current_day_cost = current_day_cost.min(ans[t] + c7);
             }
-            if current_day < start_day + 30 {
+            if current_day_idx < 30 {
                 current_day_cost = current_day_cost.min(c30);
             }
-            for t in (if current_day < start_day + 30 { 0 } else { current_day_idx - 30 })..current_day_idx {
+            for t in (if current_day_idx < 30 { 0 } else { current_day_idx - 30 })..current_day_idx {
                 current_day_cost = current_day_cost.min(ans[t] + c30);
             }
             ans[current_day_idx] = current_day_cost;
