95e024d4f34c536c3c51e6699d9bce441c19e7ee
Shane Xu <xusheng0711@gmail.com>
2024-12-31 15:35:43 +0800

    refine

--- src/bin/minimum-cost-for-tickets.rs
+++ src/bin/minimum-cost-for-tickets.rs
@@ -24,24 +24,24 @@ impl Solution {
         for i in 1..days.len() {
             let prev_day = days[i - 1] as usize;
             let prev_day_cost = ans[prev_day - start_day];
-            let day = days[i] as usize;
-            for j in (prev_day + 1 - start_day)..(day - start_day) {
+            let current_day = days[i] as usize;
+            for j in (prev_day + 1 - start_day)..(current_day - start_day) {
                 ans[j] = prev_day_cost;
             }
-            let mut cost = ans[day - start_day - 1] + c1;
-            if day < start_day + 7 {
-                cost = cost.min(c7);
+            let mut current_day_cost = ans[current_day - start_day - 1] + c1;
+            if current_day < start_day + 7 {
+                current_day_cost = current_day_cost.min(c7);
             }
-            for t in (if day < start_day + 7 { 0 } else { day - start_day - 7 })..=(day - start_day - 1) {
-                cost = cost.min(ans[t] + c7);
+            for t in (if current_day < start_day + 7 { 0 } else { current_day - start_day - 7 })..=(current_day - start_day - 1) {
+                current_day_cost = current_day_cost.min(ans[t] + c7);
             }
-            if day < start_day + 30 {
-                cost = cost.min(c30);
+            if current_day < start_day + 30 {
+                current_day_cost = current_day_cost.min(c30);
             }
-            for t in (if day < start_day + 30 { 0 } else { day - start_day - 30 })..=(day - start_day - 1) {
-                cost = cost.min(ans[t] + c30);
+            for t in (if current_day < start_day + 30 { 0 } else { current_day - start_day - 30 })..=(current_day - start_day - 1) {
+                current_day_cost = current_day_cost.min(ans[t] + c30);
             }
-            ans[day - start_day] = cost;
+            ans[current_day - start_day] = current_day_cost;
         }
         ans[ans.len() - 1]
     }
