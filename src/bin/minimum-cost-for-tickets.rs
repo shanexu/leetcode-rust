c29f14b5f2e3e84e1aa05ff25491d85ab3580f6a
Shane Xu <xusheng0711@gmail.com>
2024-12-31 15:37:34 +0800

    refine

--- src/bin/minimum-cost-for-tickets.rs
+++ src/bin/minimum-cost-for-tickets.rs
@@ -25,23 +25,24 @@ impl Solution {
             let prev_day = days[i - 1] as usize;
             let prev_day_cost = ans[prev_day - start_day];
             let current_day = days[i] as usize;
-            for j in (prev_day + 1 - start_day)..(current_day - start_day) {
+            let current_day_idx = current_day - start_day;
+            for j in (prev_day + 1 - start_day)..(current_day_idx) {
                 ans[j] = prev_day_cost;
             }
-            let mut current_day_cost = ans[current_day - start_day - 1] + c1;
+            let mut current_day_cost = ans[current_day_idx - 1] + c1;
             if current_day < start_day + 7 {
                 current_day_cost = current_day_cost.min(c7);
             }
-            for t in (if current_day < start_day + 7 { 0 } else { current_day - start_day - 7 })..=(current_day - start_day - 1) {
+            for t in (if current_day < start_day + 7 { 0 } else { current_day_idx - 7 })..current_day_idx {
                 current_day_cost = current_day_cost.min(ans[t] + c7);
             }
             if current_day < start_day + 30 {
                 current_day_cost = current_day_cost.min(c30);
             }
-            for t in (if current_day < start_day + 30 { 0 } else { current_day - start_day - 30 })..=(current_day - start_day - 1) {
+            for t in (if current_day < start_day + 30 { 0 } else { current_day_idx - 30 })..current_day_idx {
                 current_day_cost = current_day_cost.min(ans[t] + c30);
             }
-            ans[current_day - start_day] = current_day_cost;
+            ans[current_day_idx] = current_day_cost;
         }
         ans[ans.len() - 1]
     }
