32d092616344a02e76cbf9f28bb1ea6769f6da09
Shane Xu <xusheng0711@gmail.com>
2025-01-03 08:46:58 +0800

    add solution

---
+++ src/bin/smallest-range-i.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn smallest_range_i(nums: Vec<i32>, k: i32) -> i32 {
+        let mut min = i32::MAX;
+        let mut max = i32::MIN;
+        for v in nums {
+            min = min.min(v);
+            max = max.max(v);
+        }
+        let range = max - min;
+        if range <= 2 * k {
+            0
+        } else {
+            range - 2 * k
+        }
+    }
+}
