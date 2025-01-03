08a121efe540c32ff7c45819d9e950e30ccafe1f
Shane Xu <xusheng0711@gmail.com>
2025-01-03 12:41:00 +0800

    add solution

--- src/bin/combinations.rs
+++ src/bin/combinations.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("{:?}", Solution::combine(6, 3))
+    println!("{:?}", Solution2::combine(6, 3))
 }
 
 struct Solution;
@@ -22,6 +22,29 @@ impl Solution {
                 stack.push(c);
             }
         }
+        ans
+    }
+}
+
+struct Solution2;
+
+impl Solution2 {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        fn helper(comb: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>, n:i32, k: i32) {
+            if comb.len() == k as usize {
+                ans.push(comb.clone());
+                return;
+            }
+            let prev = *comb.last().unwrap_or(&0);
+            for i in (prev + 1)..=(n - (k - comb.len() as i32) + 1) {
+                comb.push(i);
+                helper(comb, ans, n, k);
+                comb.pop();
+            }
+        }
+        let mut ans = vec![];
+        let mut comb = Vec::with_capacity(k as usize);
+        helper(&mut comb, &mut ans, n, k);
         ans
     }
 }
