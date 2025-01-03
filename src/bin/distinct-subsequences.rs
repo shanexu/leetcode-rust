2b9c25c7aa9e79e9d7655dd886f151f8670cbdd1
Shane Xu <xusheng0711@gmail.com>
2025-01-03 22:55:40 +0800

    add solution

--- src/bin/distinct-subsequences.rs
+++ src/bin/distinct-subsequences.rs
@@ -1,19 +1,27 @@
 fn main() {
-    println!("{}", Solution::num_distinct("rabbbit".to_string(), "rabbit".to_string()));
-    println!("{}", Solution::num_distinct("babgbag".to_string(), "bag".to_string()));
+    // println!(
+    //     "{}",
+    //     Solution2::num_distinct("rabbbit".to_string(), "rabbit".to_string())
+    // );
+    // println!(
+    //     "{}",
+    //     Solution2::num_distinct("babgba".to_string(), "ba".to_string())
+    // );
+    // println!(
+    //     "{}",
+    //     Solution2::num_distinct("aabbcc".to_string(), "abc".to_string())
+    // );
+    println!(
+        "{}",
+        Solution2::num_distinct("ddd".to_string(), "dd".to_string())
+    );
 }
 
 struct Solution;
 
 impl Solution {
     pub fn num_distinct(s: String, t: String) -> i32 {
-        fn helper(
-            s: &[u8],
-            t: &[u8],
-            i: usize,
-            j: usize,
-            memo: &mut Vec<Vec<i32>>,
-        ) -> i32 {
+        fn helper(s: &[u8], t: &[u8], i: usize, j: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
             if j == t.len() {
                 return 1;
             }
@@ -33,6 +41,38 @@ impl Solution {
         }
         let s = s.as_bytes();
         let t = t.as_bytes();
-        helper(s, t, 0, 0, &mut vec![vec![-1; t.len()];s.len()])
+        helper(s, t, 0, 0, &mut vec![vec![-1; t.len()]; s.len()])
+    }
+}
+
+struct Solution2;
+
+impl Solution2 {
+    pub fn num_distinct(s: String, t: String) -> i32 {
+        let s = s.as_bytes();
+        let t = t.as_bytes();
+        let m = s.len();
+        let n = t.len();
+        let mut dp = vec![0; m + 1];
+        for i in 0..=m {
+            dp[i] = 1;
+        }
+        for j in 1..=n {
+            let mut left = 0;
+            let mut top_left = dp[0];
+            dp[0] = 0;
+            for i in 1..=m {
+                (top_left, dp[i]) = (
+                    dp[i],
+                    if s[i - 1] == t[j - 1] {
+                        left + top_left
+                    } else {
+                        left
+                    },
+                );
+                left = dp[i];
+            }
+        }
+        dp[m]
     }
 }
