1ef11f33a0c5828476817055c2e667a37a27775e
Shane Xu <xusheng0711@gmail.com>
2025-01-03 10:26:18 +0800

    add solution

---
+++ src/bin/sort-colors.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn sort_colors(nums: &mut Vec<i32>) {
+        let mut counter: Vec<usize> = vec![0; 3];
+        for x in nums.iter() {
+            counter[*x as usize] += 1;
+        }
+        let mut i = 0;
+        for (color, &count) in counter.iter().enumerate() {
+            for k in i..(i + count) {
+                nums[k] = color as i32;
+            }
+            i = i + count;
+        }
+    }
+}
