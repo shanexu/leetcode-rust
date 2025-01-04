d9b0853f91eb35c536f75d773772eda2b6d8fdc3
Shane Xu <xusheng0711@gmail.com>
2025-01-04 23:43:02 +0800

    add solution

---
+++ src/bin/number-of-substrings-with-only-1s.rs
@@ -0,0 +1,28 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn num_sub(s: String) -> i32 {
+        let mut ones: i64 = 0;
+        let mut ans: i64 = 0;
+        for c in s.chars() {
+            if c == '1' {
+                ones += 1;
+            } else {
+                if ones != 0 {
+                    ans += ((1 + ones) * ones / 2) % MOD;
+                    ans %= MOD;
+                }
+                ones = 0;
+            }
+        }
+        if ones != 0 {
+            ans += ((1 + ones) * ones / 2) % MOD;
+            ans %= MOD;
+        }
+        ans as i32
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
