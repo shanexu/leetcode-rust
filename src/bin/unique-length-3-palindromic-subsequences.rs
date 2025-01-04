4b98eefe0b1c782955a7ed28b0dff9d4efa14dc8
Shane Xu <xusheng0711@gmail.com>
2025-01-04 09:25:32 +0800

    add solution

---
+++ src/bin/unique-length-3-palindromic-subsequences.rs
@@ -0,0 +1,36 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn count_palindromic_subsequence(s: String) -> i32 {
+        let s = s.as_bytes();
+        let mut first = vec![s.len(); 26];
+        let mut last = vec![0; 26];
+        for (i, &b) in s.iter().enumerate() {
+            let idx = (b - b'a') as usize;
+            first[idx] = first[idx].min(i);
+            last[idx] = i
+        }
+        let mut ans = 0i32;
+        for i in 0..26 {
+            if first[i] < last[i] {
+                let mut bitmap = 0u32;
+                for j in first[i] + 1..last[i] {
+                    bitmap |= 1 << (s[j] - b'a') as u32
+                }
+                ans += bit_count(bitmap) as i32;
+            }
+        }
+        ans
+    }
+}
+
+fn bit_count(mut var0: u32) -> u32 {
+    var0 -= var0 >> 1 & 1431655765;
+    var0 = (var0 & 858993459) + (var0 >> 2 & 858993459);
+    var0 = (var0 + (var0 >> 4)) & 252645135;
+    var0 += var0 >> 8;
+    var0 += var0 >> 16;
+    var0 & 63
+}
