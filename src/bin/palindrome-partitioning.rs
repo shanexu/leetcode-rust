991a2ffcb8e5741f6a0dd09649954cb3dc5b6e47
Shane Xu <xusheng0711@gmail.com>
2025-01-05 17:54:36 +0800

    add solution

--- src/bin/palindrome-partitioning.rs
+++ src/bin/palindrome-partitioning.rs
@@ -1,61 +1,46 @@
 fn main() {
-    println!("{:?}", Solution::partition("abbab".to_string()));
+    println!("{:?}", Solution::partition("aab".to_string()));
 }
 
 struct Solution;
 
-use std::collections::{HashMap, HashSet};
 impl Solution {
     pub fn partition(s: String) -> Vec<Vec<String>> {
-        fn helper<'a>(
+        fn dfs<'a>(
+            start: usize,
             s: &'a [u8],
-            memo: &mut HashMap<&'a [u8], HashSet<Vec<String>>>,
-        ) -> HashSet<Vec<String>> {
-            if s.len() == 0 {
-                unreachable!();
+            dp: &Vec<Vec<bool>>,
+            current: &mut Vec<&'a [u8]>,
+            ans: &mut Vec<Vec<String>>,
+        ) {
+            if start == s.len() {
+                ans.push(
+                    current
+                        .iter()
+                        .map(|&s| String::from_utf8(s.to_vec()).unwrap())
+                        .collect(),
+                );
+                return;
             }
-            if s.len() == 1 {
-                let mut res = HashSet::new();
-                res.insert(vec![String::from_utf8_lossy(s).to_string()]);
-                return res;
-            }
-            if let Some(res) = memo.get(s) {
-                return res.clone();
-            }
-            let mut res = HashSet::new();
-            res.insert(s.iter().map(|&b| String::from(char::from(b))).collect());
-            for k in 1..s.len() {
-                let prev = helper(&s[..k], memo);
-                let post = helper(&s[k..], memo);
-                for vec1 in prev.iter() {
-                    for vec2 in post.iter() {
-                        let merged: Vec<String> =
-                            vec1.iter().cloned().chain(vec2.iter().cloned()).collect();
-                        res.insert(merged);
-                    }
+            for i in start..s.len() {
+                if dp[start][i] {
+                    current.push(&s[start..=i]);
+                    dfs(i + 1, s, dp, current, ans);
+                    current.pop();
                 }
             }
-            if is_palindrome(s) {
-                res.insert(vec![String::from_utf8_lossy(s).to_string()]);
-            }
-            memo.insert(s, res.clone());
-            res
         }
-        helper(s.as_bytes(), &mut HashMap::new())
-            .into_iter()
-            .collect()
-    }
-}
 
-fn is_palindrome(s: &[u8]) -> bool {
-    let mut start = 0;
-    let mut end = s.len() - 1;
-    while start < end {
-        if s[start] != s[end] {
-            return false;
+        let s = s.as_bytes();
+        let n = s.len();
+        let mut dp = vec![vec![true; n]; n];
+        for i in (0..n).rev() {
+            for j in (i + 1)..n {
+                dp[i][j] = s[i] == s[j] && dp[i + 1][j - 1];
+            }
         }
-        start += 1;
-        end -= 1;
+        let mut ans = vec![];
+        dfs(0, s, &dp, &mut vec![], &mut ans);
+        ans
     }
-    true
 }
