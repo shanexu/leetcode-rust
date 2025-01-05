150d9f9903e51b0d5c7c4cef2903b02e1c26a4df
Shane Xu <xusheng0711@gmail.com>
2025-01-05 20:17:58 +0800

    add solution

---
+++ src/bin/ugly-number.rs
@@ -0,0 +1,17 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    fn is_ugly(mut n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+        for &factor in [2, 3, 5].iter() {
+            while n % factor == 0 {
+                n /= factor;
+            }
+        }
+        n == 1
+    }
+}
