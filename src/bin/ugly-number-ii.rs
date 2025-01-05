88caa8534f3865adcd2ffeca2f770bbdb5a66985
Shane Xu <xusheng0711@gmail.com>
2025-01-05 20:12:50 +0800

    refine

--- src/bin/ugly-number-ii.rs
+++ src/bin/ugly-number-ii.rs
@@ -8,17 +8,17 @@ struct Solution;
 impl Solution {
     pub fn nth_ugly_number(n: i32) -> i32 {
         let n = n as usize;
-        let mut nums = Vec::with_capacity(n);
-        nums.push(1);
+        let mut nums = vec![0; n];
+        nums[0] = 1;
         let mut s2 = 0usize;
         let mut s3 = 0usize;
         let mut s5 = 0usize;
-        for _ in 1..n {
+        for i in 1..n {
             let u2 = nums[s2] * 2;
             let u3 = nums[s3] * 3;
             let u5 = nums[s5] * 5;
             let u = u2.min(u3).min(u5);
-            nums.push(u);
+            nums[i] = u;
             if u2 == u {
                 s2 += 1;
             }
