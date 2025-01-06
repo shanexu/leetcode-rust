ac20dec6ba7f60ab979a0a7bcf7416743bb20a84
Shane Xu <xusheng0711@gmail.com>
2025-01-06 11:28:50 +0800

    refine

--- src/bin/interleaving-string.rs
+++ src/bin/interleaving-string.rs
@@ -69,3 +69,31 @@ impl Solution {
         ) == 1
     }
 }
+
+struct Solution2;
+
+impl Solution2 {
+    pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
+        let s1 = s1.as_bytes();
+        let s2 = s2.as_bytes();
+        let s3 = s3.as_bytes();
+        if s3.len() != s1.len() + s2.len() {
+            return false;
+        }
+        let m = s1.len();
+        let n = s2.len();
+        let mut dp = vec![vec![false; n + 1]; m + 1];
+        dp[0][0] = true;
+        for i in 0..=m {
+            for j in 0..=n {
+                if i >= 1 && s1[i - 1] == s3[i + j - 1] {
+                    dp[i][j] |= dp[i - 1][j];
+                }
+                if j >= 1 && s2[j - 1] == s3[i + j - 1] {
+                    dp[i][j] |= dp[i][j - 1];
+                }
+            }
+        }
+        dp[m][n]
+    }
+}
