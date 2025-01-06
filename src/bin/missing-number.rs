566a26eaf27d7c573787a7d89c0d124edf2d3cf9
Shane Xu <xusheng0711@gmail.com>
2025-01-06 14:13:13 +0800

    add solution

---
+++ src/bin/missing-number.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("{}", Solution::missing_number(vec![3,0,1]))
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let n = nums.len() as i32;
+        let sum: i32 = nums.iter().sum();
+        (1 + n) * n / 2 - sum
+    }
+}
+
+struct Solution2;
+
+impl Solution2 {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let mut x = 0;
+        let n = nums.len() as i32;
+        for (i, &t) in nums.iter().enumerate() {
+            x ^= t;
+            x ^= i as i32;
+        }
+        x ^ n
+    }
+}
