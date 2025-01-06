5f4cae44870f821b1a7d09f7eb6794ad932fbea4
Shane Xu <xusheng0711@gmail.com>
2025-01-06 10:29:08 +0800

    refine

--- src/bin/interleaving-string.rs
+++ src/bin/interleaving-string.rs
@@ -26,14 +26,18 @@ impl Solution {
             if s1.len() == i1 && s2.len() == i2 && s3.len() == i3 {
                 return true;
             }
-            if s1.len() == i1 {
-                return s3[i3..] == s2[i2..];
-            }
-            if s2.len() == i2 {
-                return s3[i3..] == s1[i1..];
-            }
             if let Some(&v) = memo.get(&(i1, i2, i3)) {
                 return v;
+            }
+            if s1.len() == i1 {
+                let ans = s3[i3..] == s2[i2..];
+                memo.insert((i1, i2, i3), ans);
+                return ans;
+            }
+            if s2.len() == i2 {
+                let ans = s3[i3..] == s1[i1..];
+                memo.insert((i1, i2, i3), ans);
+                return ans
             }
             if s3[i3] == s1[i1] {
                 if helper(s1, s2, s3, i1 + 1, i2, i3 + 1, memo) {
