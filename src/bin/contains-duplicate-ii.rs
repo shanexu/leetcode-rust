d680a02b171e92f95aff1e2886a47321cec8e536
Shane Xu <xusheng0711@gmail.com>
2025-01-06 14:00:47 +0800

    add solution

---
+++ src/bin/contains-duplicate-ii.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let k = k as usize;
+        let mut map = std::collections::HashMap::with_capacity(nums.len());
+        for (j, &v) in nums.iter().enumerate() {
+            if let Some(i) = map.insert(v, j) {
+                if j - i <= k {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
