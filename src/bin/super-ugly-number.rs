ac98d81011d222ac86f2ef93e8fbdd9f1037e2a9
Shane Xu <xusheng0711@gmail.com>
2025-01-07 10:13:47 +0800

    refine

--- src/bin/super-ugly-number.rs
+++ src/bin/super-ugly-number.rs
@@ -1,32 +1,34 @@
 fn main() {
     println!(
         "{}",
-        Solution::nth_super_ugly_number(5911, vec![2,3,5,7])
+        Solution::nth_super_ugly_number(5911, vec![2, 3, 5, 7])
     );
 }
-
 struct Solution;
 
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 impl Solution {
     pub fn nth_super_ugly_number(n: i32, primes: Vec<i32>) -> i32 {
         let n = n as usize;
         let mut ans: Vec<i64> = Vec::with_capacity(n);
+        let mut heap = BinaryHeap::new();
         ans.push(1);
-        let mut set = std::collections::BTreeSet::new();
         for &p in primes.iter() {
-            set.insert((p as i64 * ans[0], p, 0));
+            heap.push(Reverse((p as i64 * ans[0], p, 0)));
         }
-        let (mut value, mut prime, mut idx) = set.pop_first().unwrap();
-        for i in 1..n {
+        let Reverse((mut value, mut prime, mut idx)) = heap.pop().unwrap();
+        for _ in 1..n {
             let last_value = value;
             ans.push(last_value);
-            set.insert((prime as i64 * ans[idx + 1], prime, idx + 1));
-            while !set.is_empty() {
-                (value, prime, idx) = set.pop_first().unwrap();
+            heap.push(Reverse((prime as i64 * ans[idx + 1], prime, idx + 1)));
+            while !heap.is_empty() {
+                let Reverse(t) = heap.pop().unwrap();
+                (value, prime, idx) = t;
                 if value != last_value {
                     break;
                 }
-                set.insert((prime as i64 * ans[idx + 1], prime, idx + 1));
+                heap.push(Reverse((prime as i64 * ans[idx + 1], prime, idx + 1)));
             }
         }
         ans[n - 1] as i32
