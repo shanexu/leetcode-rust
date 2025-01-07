da9a9e9af76ca3eea250f6fcd11acb4c09473d49
Shane Xu <xusheng0711@gmail.com>
2025-01-07 09:22:20 +0800

    add solution

---
+++ src/bin/maximum-product-subarray.rs
@@ -0,0 +1,24 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut ans = i32::MIN;
+        let mut left = 0;
+        let mut right = 0;
+        for i in 0..n {
+            if left == 0 {
+                left = 1;
+            }
+            if right == 0 {
+                right = 1;
+            }
+            left *= nums[i];
+            right *= nums[n - i - 1];
+            ans = ans.max(left).max(right);
+        }
+        ans
+    }
+}
