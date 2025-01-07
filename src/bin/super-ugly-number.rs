2bf4af36f40bf6b291e31a59f2c2136ffe96849d
Shane Xu <xusheng0711@gmail.com>
2025-01-07 11:03:46 +0800

    refine

--- src/bin/super-ugly-number.rs
+++ src/bin/super-ugly-number.rs
@@ -4,37 +4,37 @@ fn main() {
         Solution::nth_super_ugly_number(5911, vec![2, 3, 5, 7])
     );
 }
+
 struct Solution;
 
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 impl Solution {
     pub fn nth_super_ugly_number(n: i32, primes: Vec<i32>) -> i32 {
         let n = n as usize;
-        let mut ans: Vec<i64> = Vec::with_capacity(n);
-        let mut heap: BinaryHeap<Reverse<(i64, i64, usize)>> = BinaryHeap::with_capacity(primes.len());
+        let k = primes.len();
+        let mut indices = vec![0; k];
+        let primes: Vec<u64> = primes.iter().map(|&x| x as u64).collect();
+        let mut candidate: Vec<u64> = primes.clone();
+        let mut min_value = u64::MAX;
+        let mut min_indices: Vec<usize> = vec![0; k];
+        let mut ans: Vec<u64> = Vec::with_capacity(n);
         ans.push(1);
-        for &p in primes.iter() {
-            heap.push(Reverse((p as i64 * ans[0], p as i64, 0)));
-        }
         for _ in 1..n {
-            let mut last_value = 0;
-            loop {
-                let mut peek = heap.peek_mut().unwrap();
-                if last_value != 0 {
-                    if peek.0 .0 != last_value {
-                        break;
-                    }
-                } else {
-                    last_value = peek.0 .0;
-                    ans.push(last_value);
+            for i in 0..k {
+                if min_value > candidate[i] {
+                    min_value = candidate[i];
+                    min_indices.clear();
+                    min_indices.push(i);
+                } else if min_value == candidate[i] {
+                    min_indices.push(i);
                 }
-                *peek = Reverse((
-                    peek.0 .1 * ans[peek.0 .2 + 1],
-                    peek.0 .1,
-                    peek.0 .2 + 1,
-                ));
             }
+            ans.push(min_value);
+            for &idx in min_indices.iter() {
+                indices[idx] += 1;
+                candidate[idx] = ans[indices[idx]] * primes[idx];
+            }
+            min_indices.clear();
+            min_value = u64::MAX;
         }
         ans[n - 1] as i32
     }
