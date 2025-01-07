562ba9da2c83211809e76a574f80318c2d00d6cc
Shane Xu <xusheng0711@gmail.com>
2025-01-07 10:34:48 +0800

    refine

--- src/bin/super-ugly-number.rs
+++ src/bin/super-ugly-number.rs
@@ -12,27 +12,22 @@ impl Solution {
     pub fn nth_super_ugly_number(n: i32, primes: Vec<i32>) -> i32 {
         let n = n as usize;
         let mut ans: Vec<i64> = Vec::with_capacity(n);
-        let mut heap = BinaryHeap::new();
+        let mut heap = BinaryHeap::with_capacity(primes.len());
         ans.push(1);
         for &p in primes.iter() {
             heap.push(Reverse((p as i64 * ans[0], p, 0)));
         }
         for _ in 1..n {
-            let last_value;
-            {
-                let mut peek = heap.peek_mut().unwrap();
-                last_value = peek.0 .0;
-                ans.push(last_value);
-                *peek = Reverse((
-                    peek.0 .1 as i64 * ans[peek.0 .2 + 1],
-                    peek.0 .1,
-                    peek.0 .2 + 1,
-                ));
-            }
+            let mut last_value = 0;
             loop {
                 let mut peek = heap.peek_mut().unwrap();
-                if peek.0 .0 != last_value {
-                    break;
+                if last_value != 0 {
+                    if peek.0 .0 != last_value {
+                        break;
+                    }
+                } else {
+                    last_value = peek.0 .0;
+                    ans.push(last_value);
                 }
                 *peek = Reverse((
                     peek.0 .1 as i64 * ans[peek.0 .2 + 1],
