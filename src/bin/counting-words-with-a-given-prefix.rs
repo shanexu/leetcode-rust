dbe6dd0c2a9ac9f5c043b808a2232ea5681b9e20
Shane Xu <xusheng0711@gmail.com>
2025-01-09 08:29:24 +0800

    add solution

---
+++ src/bin/counting-words-with-a-given-prefix.rs
@@ -0,0 +1,9 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn prefix_count(words: Vec<String>, pref: String) -> i32 {
+        words.iter().filter(|&w| w.starts_with(&pref)).count() as i32
+    }
+}
