0611d487d4601dfa9c38175d8ded3deb9740ebaa
Shane Xu <xusheng0711@gmail.com>
2025-01-09 12:33:08 +0800

    add solution

---
+++ src/bin/design-hashset/Main.java
@@ -0,0 +1,66 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
+class MyHashSet {
+    private static final int CAPACITY = 10000;
+    private Node[] nodes;
+
+    public MyHashSet() {
+        nodes = new Node[CAPACITY];
+    }
+
+    public void add(int key) {
+        int r = key % CAPACITY;
+        Node curr = nodes[r];
+        while (curr != null) {
+            if (curr.key == key) {
+                return;
+            }
+            curr = curr.next;
+        }
+        curr = new Node(key);
+        curr.next = nodes[r];
+        nodes[r] = curr;
+    }
+
+    public void remove(int key) {
+        int r = key % CAPACITY;
+        Node dummy = new Node(-1);
+        Node prev = dummy;
+        Node curr = nodes[r];
+        prev.next = curr;
+        while (curr != null) {
+            if (curr.key == key) {
+                prev.next = curr.next;
+                break;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        nodes[r] = dummy.next;
+    }
+
+    public boolean contains(int key) {
+        int r = key % CAPACITY;
+        Node curr = nodes[r];
+        while (curr != null) {
+            if (curr.key == key) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+}
+
+class Node {
+    int key;
+    Node next;
+
+    public Node(int key) {
+        this.key = key;
+    }
+}
