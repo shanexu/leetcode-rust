9243543676a22d72a3efe82a67ac8c28e0f4cf50
Shane Xu <xusheng0711@gmail.com>
2025-01-09 12:27:08 +0800

    add solution

---
+++ src/bin/design-hashmap/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+    public static void main(String[] args) {
+        MyHashMap map = new MyHashMap();
+        map.put(1, 10);
+        System.out.println(map.get(1));
+        map.put(1, 11);
+        System.out.println(map.get(1));
+        map.remove(1);
+        System.out.println(map.get(1));
+        map.put(1, 10);
+        map.put(10001, 11);
+        System.out.println(map.get(1));
+        System.out.println(map.get(10001));
+        map.remove(1);
+        System.out.println(map.get(1));
+        System.out.println(map.get(10001));
+    }
+}
+
+class MyHashMap {
+    private static final int CAPACITY = 10000;
+    private Node[] nodes;
+
+    public MyHashMap() {
+        this.nodes = new Node[CAPACITY];
+    }
+
+    public void put(int key, int value) {
+        int r = key % CAPACITY;
+        Node curr = nodes[r];
+        while (curr != null) {
+            if (curr.key == key) {
+                curr.value = value;
+                break;
+            }
+            curr = curr.next;
+        }
+        if (curr == null) {
+            curr = new Node(key, value);
+            curr.next = nodes[r];
+            nodes[r] = curr;
+        }
+    }
+
+    public int get(int key) {
+        int r = key % CAPACITY;
+        Node curr = nodes[r];
+        while (curr != null) {
+            if (curr.key == key) {
+                return curr.value;
+            }
+            curr = curr.next;
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int r = key % CAPACITY;
+        Node dummy = new Node(-1, -1);
+        Node prev = dummy;
+        Node curr = nodes[r];
+        prev.next = curr;
+        while (curr != null) {
+            if (curr.key == key) {
+                prev.next = curr.next;
+                break;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        nodes[r] = dummy.next;
+    }
+}
+
+class Node {
+    int key;
+    int value;
+    Node next;
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
