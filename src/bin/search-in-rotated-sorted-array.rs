786c26c8e7764905e2b815f9658cf84a6150a2a4
Shane Xu <xusheng0711@gmail.com>
2025-01-10 22:16:38 +0800

    add solution

--- src/bin/search-in-rotated-sorted-array.rs
+++ src/bin/search-in-rotated-sorted-array.rs
@@ -1,45 +1,42 @@
 fn main() {
     println!("{}", Solution::search(vec![1, 2, 3, 4, 5, 6, 0], 2));
     println!("{}", Solution::search(vec![4, 5, 6, 7, 0, 1, 2], 3));
-    println!("{}", Solution::search(vec![3, 1, 2], 2));
+    println!("{}", Solution::search(vec![2, 3, 0, 1], 2));
 }
 
 struct Solution;
 
+use std::cmp::Ordering::{Equal, Greater, Less};
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        if nums.len() == 1 {
-            return if nums[0] == target { 0 } else { -1 };
+        let n = nums.len();
+        let mut lo = 0;
+        let mut hi = n - 1;
+        while lo < hi {
+            if nums[lo] < nums[hi] {
+                break;
+            }
+            let mid = (lo + hi) / 2;
+            if nums[mid] > nums[hi] {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
         }
-        let k = find_k(&nums, 0, nums.len() - 1);
-        let (l, r) = nums.split_at(k);
-        l.binary_search(&target)
-            .or_else(|_| r.binary_search(&target).map(|i| i + k))
-            .map(|i| i as i32)
-            .unwrap_or(-1)
-    }
-}
-
-fn find_k(nums: &Vec<i32>, begin_idx: usize, end_idx: usize) -> usize {
-    if is_increasing(nums, begin_idx, end_idx) {
-        return begin_idx;
-    }
-    let k = (begin_idx + end_idx + 1) / 2;
-    let l = is_increasing(nums, begin_idx, k - 1);
-    let r = is_increasing(nums, k, end_idx);
-    if l && r {
-        return k;
-    } else if !r {
-        return find_k(nums, k, end_idx);
-    } else {
-        return find_k(nums, begin_idx, k - 1);
-    }
-}
-
-fn is_increasing(nums: &Vec<i32>, begin_idx: usize, end_idx: usize) -> bool {
-    if begin_idx == end_idx {
-        true
-    } else {
-        nums[begin_idx] < nums[end_idx]
+        let mut size = n;
+        let mut left = 0;
+        let mut right = size;
+        while left < right {
+            let mid = left + size / 2;
+            let mid_val = nums[(mid + lo) % n];
+            let cmp = mid_val.cmp(&target);
+            left = if cmp == Less { mid + 1 } else { left };
+            right = if cmp == Greater { mid } else { right };
+            if cmp == Equal {
+                return ((mid + lo) % n) as i32;
+            }
+            size = right - left;
+        }
+        -1
     }
 }
