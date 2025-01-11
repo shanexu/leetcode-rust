611196e3420787e83c312b602060282b4929de1f
Shane Xu <xusheng0711@gmail.com>
2025-01-11 15:22:17 +0800

    refine

--- src/bin/subsets-ii.rs
+++ src/bin/subsets-ii.rs
@@ -7,23 +7,22 @@ struct Solution;
 impl Solution {
     pub fn subsets_with_dup(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut ans = vec![];
-        let mut map = std::collections::BTreeMap::new();
-        let mut total = 1;
+        let mut freq: Vec<usize> = vec![1; 21];
         for num in nums {
-            let v = map.entry(num).and_modify(|x| *x += 1).or_insert(1);
-            if *v >= 1 {
-                total = total / *v * (*v + 1)
-            }
+            freq[(num + 10) as usize] += 1;
         }
-        let pairs: Vec<(i32, usize)> = map.iter().map(|(k, v)| (*k, *v)).collect();
+        let total = freq.iter().product();
         for mut i in 0..total {
             let mut s = vec![];
             let mut c = 0usize;
             while i > 0 {
-                let (k, v) = pairs[c];
-                let r = i % (v + 1);
-                s.extend(vec![k; r]);
-                i /= v + 1;
+                let f = freq[c];
+                if f != 1 {
+                    let r = i % f;
+                    s.extend(vec![c as i32 - 10; r]);
+                    i /= f;
+
+                }
                 c += 1;
             }
             ans.push(s);
