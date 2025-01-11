f170013e82083d3d09a2ce50a43aff39f10d7f0f
Shane Xu <xusheng0711@gmail.com>
2025-01-11 09:14:12 +0800

    add solution

---
+++ src/bin/construct-k-palindrome-strings.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("{}", Solution::can_construct("annabelle".to_string(), 2));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn can_construct(s: String, k: i32) -> bool {
+        let mut freq = vec![0; 26];
+        let s = s.as_bytes();
+        for &b in s {
+            freq[(b - b'a') as usize] += 1;
+        }
+        let mut p = 0; // 奇数
+        let mut q = 0; // 偶数
+        for f in freq {
+            if f == 0 {
+                continue;
+            }
+            if f & 1 == 1 {
+                p += 1;
+            }
+            q += f / 2;
+        }
+        k >= p && k <= p + 2 * q
+    }
+}
