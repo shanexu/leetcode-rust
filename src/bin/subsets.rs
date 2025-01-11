64d793d35d7e41a756b337aec6d3e5199affed2a
Shane Xu <xusheng0711@gmail.com>
2025-01-11 12:08:48 +0800

    refine

--- src/bin/subsets.rs
+++ src/bin/subsets.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("{:?}", Solution::subsets(vec![1, 2, 3]));
+    println!("{:?}", Solution2::subsets(vec![1, 2, 3]));
 }
 
 struct Solution;
@@ -18,5 +19,27 @@ impl Solution {
             x
         }
         helper(&nums, 0, &mut vec![])
+    }
+}
+
+struct Solution2;
+impl Solution2 {
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let n = nums.len();
+        let m = 1 << n;
+        let mut ans = Vec::with_capacity(m);
+        for mut i in 0..m {
+            let mut s = vec![];
+            let mut j = 0;
+            while i > 0 {
+                if i & 1 == 1 {
+                    s.push(nums[j]);
+                }
+                j += 1;
+                i >>= 1;
+            }
+            ans.push(s);
+        }
+        ans
     }
 }
