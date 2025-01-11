a186f1a6d1e1e7c1cbeac60d899293135b936348
Shane Xu <xusheng0711@gmail.com>
2025-01-11 15:47:21 +0800

    add solution

---
+++ src/bin/candy.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("{}", Solution::candy(vec![1, 0, 2]));
+    println!("{}", Solution::candy(vec![1, 2, 2]));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn candy(ratings: Vec<i32>) -> i32 {
+        let n = ratings.len();
+        let mut candies = vec![1; n];
+        for i in 1..n {
+            if ratings[i] > ratings[i - 1] {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for i in (0..(n - 1)).rev() {
+            if ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1] {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+        candies.iter().sum()
+    }
+}
