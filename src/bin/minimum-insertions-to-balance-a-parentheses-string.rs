e154c277f2cfdd5886450e6d68aef9dde4a2a5e3
Shane Xu <xusheng0711@gmail.com>
2025-01-12 20:04:22 +0800

    add solution

--- src/bin/minimum-insertions-to-balance-a-parentheses-string.rs
+++ src/bin/minimum-insertions-to-balance-a-parentheses-string.rs
@@ -15,46 +15,32 @@ struct Solution;
 impl Solution {
     pub fn min_insertions(s: String) -> i32 {
         let s = s.as_bytes();
-        let mut stack = vec![];
         let mut ans = 0;
         let mut i = 0;
         let n = s.len();
+        let mut open = 0;
         while i < s.len() {
-            if s[i] == b')' {
+            if s[i] == b'(' {
+                open += 1;
+            } else if s[i] == b')' {
                 if i < n - 1 && s[i + 1] == b')' {
-                    match stack.last() {
-                        Some(&b'(') => {
-                            stack.pop();
-                        }
-                        _ => stack.push(b']'),
+                    if open > 0 {
+                        open -= 1;
+                        i += 1;
+                    } else {
+                        ans += 1;
+                        i += 1;
                     }
-                    i += 1;
                 } else {
-                    stack.push(s[i]);
+                    if open > 0 {
+                        open -= 1;
+                        ans += 1;
+                    } else {
+                        ans += 2;
+                    }
                 }
-            } else {
-                stack.push(s[i]);
             }
             i += 1;
-        }
-        let mut open = 0;
-        for &b in stack.iter() {
-            if b == b'(' {
-                open += 1;
-            } else if b == b')' {
-                ans += 1;
-                if open > 0 {
-                    open -= 1;
-                } else {
-                    ans += 1;
-                }
-            } else if b == b']' {
-                if open > 0 {
-                    open -= 1;
-                } else {
-                    ans += 1;
-                }
-            }
         }
         ans + open * 2
     }
