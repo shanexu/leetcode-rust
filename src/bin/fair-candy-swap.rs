96f2a58b9bb8ca73a5a522c35149c0d7ce181778
Shane Xu <xusheng0711@gmail.com>
2025-01-12 20:46:05 +0800

    add solution

---
+++ src/bin/fair-candy-swap.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("{:?}", Solution::fair_candy_swap(vec![1, 1], vec![2, 2]));
+}
+
+struct Solution;
+
+use std::collections::HashSet;
+impl Solution {
+    pub fn fair_candy_swap(alice_sizes: Vec<i32>, bob_sizes: Vec<i32>) -> Vec<i32> {
+        let s1: i32 = alice_sizes.iter().sum();
+        let s2: i32 = bob_sizes.iter().sum();
+        let d = (s2 - s1) / 2;
+        let set1 = alice_sizes.iter().cloned().collect::<HashSet<_>>();
+        let set2 = bob_sizes.iter().cloned().collect::<HashSet<_>>();
+        let mut ans = vec![0; 2];
+        for &a in set1.iter() {
+            let b = a + d;
+            if set2.contains(&b) {
+                ans[0] = a;
+                ans[1] = b;
+                break;
+            }
+        }
+        ans
+    }
+}
