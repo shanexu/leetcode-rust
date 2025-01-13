4770f02be5961d19462fcbc7064d71ac83456bfb
Shane Xu <xusheng0711@gmail.com>
2025-01-14 00:36:46 +0800

    add solution

---
+++ src/bin/reverse-string.rs
@@ -0,0 +1,12 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        let n = s.len();
+        for i in 0..(n / 2) {
+            s.swap(i, n - 1 - i);
+        }
+    }
+}
