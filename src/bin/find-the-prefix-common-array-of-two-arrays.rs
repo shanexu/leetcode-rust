7698d10dde6b63b368a18e222603b58d89819a79
Shane Xu <xusheng0711@gmail.com>
2025-01-14 08:26:37 +0800

    refine

--- src/bin/find-the-prefix-common-array-of-two-arrays.rs
+++ src/bin/find-the-prefix-common-array-of-two-arrays.rs
@@ -6,16 +6,16 @@ impl Solution {
     pub fn find_the_prefix_common_array(a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
         let n = a.len();
         let mut ans = vec![0; n];
-        let mut fa = vec![0; n + 1];
-        let mut fb = vec![0; n + 1];
+        let mut fa: u64 = 0;
+        let mut fb: u64 = 0;
 
         for i in 0..n {
             let ia = a[i] as usize;
             let ib = b[i] as usize;
-            fa[ia] = 1;
-            ans[i] += fb[ia];
-            fb[ib] = 1;
-            ans[i] += fa[ib];
+            fa |= 1 << ia;
+            ans[i] += ((fb >> ia) & 1) as i32;
+            fb |= 1 << ib;
+            ans[i] += ((fa >> ib) & 1) as i32;
             if i < n - 1 {
                 ans[i + 1] = ans[i];
             }
