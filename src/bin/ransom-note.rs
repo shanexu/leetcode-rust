db1b0b896cdace1440e6b82a62d2fd05bafba794
Shane Xu <xusheng0711@gmail.com>
2025-01-14 11:13:57 +0800

    add solution

---
+++ src/bin/ransom-note.rs
@@ -0,0 +1,26 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        fn freq(s: &[u8]) -> [i32; 26] {
+            let mut f = [0; 26];
+            for &b in s {
+                f[(b - b'a') as usize] += 1;
+            }
+            f
+        }
+
+        let ransom_note = ransom_note.as_bytes();
+        let magazine = magazine.as_bytes();
+        let f1 = freq(ransom_note);
+        let f2 = freq(magazine);
+        for i in 0..26 {
+            if f1[i] > f2[i] {
+                return false;
+            }
+        }
+        true
+    }
+}
