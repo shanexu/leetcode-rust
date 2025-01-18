69a4057ed1935902e5d8daa874eb41fd5f150be0
Shane Xu <xusheng0711@gmail.com>
2025-01-18 09:45:56 +0800

    add solution

---
+++ src/bin/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.rs
@@ -0,0 +1,43 @@
+fn main() {}
+
+struct Solution;
+
+use std::collections::VecDeque;
+impl Solution {
+    pub fn min_cost(grid: Vec<Vec<i32>>) -> i32 {
+        let num_rows = grid.len();
+        let num_cols = grid[0].len();
+
+        let mut visited = vec![vec![false; num_cols]; num_rows];
+        let mut queue = VecDeque::with_capacity(num_rows * num_cols);
+
+        queue.push_back((0, 0, 0));
+
+        let directions = vec![(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)];
+
+        while let Some((i, j, cost)) = queue.pop_front() {
+            if i == num_rows - 1 && j == num_cols - 1 {
+                return cost;
+            }
+
+            if visited[i][j] {
+                continue;
+            }
+
+            visited[i][j] = true;
+
+            for k in 1..=4 {
+                let (new_x, new_y) = (i as i32 + directions[k].0, j as i32 + directions[k].1);
+
+                if new_x >= 0 && new_x < num_rows as i32 && new_y >= 0 && new_y < num_cols as i32 {
+                    if grid[i][j] == k as i32 {
+                        queue.push_front((new_x as usize, new_y as usize, cost));
+                    } else {
+                        queue.push_back((new_x as usize, new_y as usize, cost + 1));
+                    }
+                }
+            }
+        }
+        unreachable!()
+    }
+}
