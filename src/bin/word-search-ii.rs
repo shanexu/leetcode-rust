6054c62bff21687a53e26167cffbab52ec324299
Shane Xu <xusheng0711@gmail.com>
2025-01-18 19:31:58 +0800

    refine

--- src/bin/word-search-ii.rs
+++ src/bin/word-search-ii.rs
@@ -1,42 +1,69 @@
-fn main() {}
+fn main() {
+    let board = vec![
+        vec!['o', 'a', 'a', 'n'],
+        vec!['e', 't', 'a', 'e'],
+        vec!['i', 'h', 'k', 'r'],
+        vec!['i', 'f', 'l', 'v'],
+    ];
+    let words = vec![
+        "oath".to_string(),
+        "pea".to_string(),
+        "eat".to_string(),
+        "rain".to_string(),
+    ];
+    println!("{:?}", Solution::find_words(board, words));
+}
 
 struct Solution;
 
 impl Solution {
     pub fn find_words(mut board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
-        let mut trie = Trie::new();
-        for (i, word) in words.iter().enumerate() {
-            trie.insert(word, i);
-        }
         let rows = board.len();
         let cols = board[0].len();
-        let mut found_words = Vec::with_capacity(words.len());
+        let mut board_freq = vec![0; 26];
+        for i in 0..rows {
+            for j in 0..cols {
+                board_freq[board[i][j] as usize - 'a' as usize] += 1;
+            }
+        }
+        let mut trie = Trie::new();
+        words.into_iter().for_each(|word| {
+            let mut board_freq = board_freq.clone();
+            for &b in word.as_bytes() {
+                if board_freq[b as usize - 'a' as usize] == 0 {
+                    return;
+                }
+                board_freq[b as usize - 'a' as usize] -= 1;
+            }
+            trie.insert(word);
+        });
+        let mut found_words = vec![];
         for i in 0..rows {
             for j in 0..cols {
                 dfs(i, j, rows, cols, &mut board, &mut trie, &mut found_words);
             }
         }
-        found_words.into_iter().map(|i| words[i].clone()).collect()
+        found_words
     }
 }
 
+#[inline(always)]
 fn dfs(
     i: usize,
     j: usize,
     rows: usize,
     cols: usize,
     board: &mut Vec<Vec<char>>,
-    mut node: &mut Trie,
-    found_words: &mut Vec<usize>,
+    tire: &mut Trie,
+    found_words: &mut Vec<String>,
 ) {
     let char_idx = board[i][j] as usize - 'a' as usize;
-    if node.children[char_idx].is_none() {
+    if tire.children[char_idx].is_none() {
         return;
     }
-    node = node.children[char_idx].as_mut().unwrap();
-    if let Some(r) = node.reference {
-        found_words.push(r);
-        node.reference = None;
+    let node = tire.children[char_idx].as_mut().unwrap();
+    if let Some(w) = node.word.take() {
+        found_words.push(w);
     }
     let temp_char = board[i][j];
     board[i][j] = '\0';
@@ -61,25 +88,30 @@ fn dfs(
         }
     }
     board[i][j] = temp_char;
+    if node.children.iter().all(|x| x.is_none()) {
+        tire.children[char_idx] = None;
+    }
 }
 
 const DIRECTIONS: &[i32] = &[-1, 0, 1, 0, -1];
 
 struct Trie {
     children: [Option<Box<Trie>>; 26],
-    reference: Option<usize>,
+    word: Option<String>,
 }
 
 const DEFAULT_TRIE_VALUE: Option<Box<Trie>> = None;
 impl Trie {
+    #[inline(always)]
     fn new() -> Self {
-        Trie {
+        Self {
             children: [DEFAULT_TRIE_VALUE; 26],
-            reference: None,
+            word: None,
         }
     }
 
-    fn insert(&mut self, word: &str, reference: usize) {
+    #[inline(always)]
+    fn insert(&mut self, word: String) {
         let mut node = self;
 
         for &character in word.as_bytes() {
@@ -92,6 +124,6 @@ impl Trie {
             node = node.children[character_index].as_deref_mut().unwrap();
         }
 
-        node.reference = Some(reference);
+        node.word = Some(word);
     }
 }
