fde4e1ad7f18b0c25518253475e932feea4606ff
Shane Xu <xusheng0711@gmail.com>
2025-01-19 17:23:39 +0800

    add solution

---
+++ src/bin/how-many-numbers-are-smaller-than-the-current-number.rs
@@ -0,0 +1,25 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
+        let mut freq = vec![0; 101];
+        for &num in nums.iter() {
+            freq[num as usize] += 1;
+        }
+        let n = nums.len();
+        let mut ans = vec![0; n];
+        let mut prev = freq[0];
+        freq[0] = 0;
+        for i in 1..=100 {
+            let temp = freq[i];
+            freq[i] = freq[i - 1] + prev;
+            prev = temp;
+        }
+        for i in 0..n {
+            ans[i] = freq[nums[i] as usize];
+        }
+        ans
+    }
+}
