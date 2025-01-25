5f958d97b28405c3e1a99090aadcb9a8d57cb396
Shane Xu <xusheng0711@gmail.com>
2025-01-25 13:25:56 +0800

    refine

--- src/bin/make-lexicographically-smallest-array-by-swapping-elements.rs
+++ src/bin/make-lexicographically-smallest-array-by-swapping-elements.rs
@@ -3,6 +3,10 @@ fn main() {
         "{:?}",
         Solution::lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3)
     );
+    println!(
+        "{:?}",
+        Solution2::lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3)
+    );
 }
 
 struct Solution;
@@ -32,6 +36,38 @@ impl Solution {
         let offset = nums.len() - is.len();
         for k in 0..is.len() {
             ans[is[k]] = nums[offset + k].0;
+        }
+        ans
+    }
+}
+
+struct Solution2;
+
+impl Solution2 {
+    pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> {
+        let n = nums.len();
+        let mut sorted: Vec<usize> = (0..n).collect();
+        sorted.sort_by(|i, j| nums[*i].cmp(&nums[*j]));
+        let mut ans = vec![0; n];
+        let mut prev = nums[sorted[0]];
+        let mut is = vec![];
+        for (j, &i) in sorted.iter().enumerate() {
+            let num = nums[i];
+            if num - prev > limit {
+                is.sort();
+                let offset = j - is.len();
+                for k in 0..is.len() {
+                    ans[is[k]] = nums[sorted[offset + k]];
+                }
+                is.clear()
+            }
+            is.push(i);
+            prev = num;
+        }
+        is.sort();
+        let offset = nums.len() - is.len();
+        for k in 0..is.len() {
+            ans[is[k]] = nums[sorted[offset + k]];
         }
         ans
     }
