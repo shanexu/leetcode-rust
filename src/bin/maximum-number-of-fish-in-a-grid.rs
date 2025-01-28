73653c7d8763f6952f64ca7f843c3cbc1af8bb90
Shane Xu <xusheng0711@gmail.com>
2025-01-28 09:48:25 +0800

    refine

--- src/bin/maximum-number-of-fish-in-a-grid.rs
+++ src/bin/maximum-number-of-fish-in-a-grid.rs
@@ -7,22 +7,22 @@ struct Solution;
 use std::collections::VecDeque;
 impl Solution {
     pub fn find_max_fish(grid: Vec<Vec<i32>>) -> i32 {
+        let mut grid = grid;
         let mut ans = 0;
         let rows = grid.len();
         let cols = grid[0].len();
-        let mut visited = vec![vec![false; cols]; rows];
         let mut queue = VecDeque::new();
         for i in 0..rows {
             for j in 0..cols {
-                if grid[i][j] > 0 && !visited[i][j] {
+                if grid[i][j] > 0 {
                     let mut s = 0;
                     queue.push_back((i, j));
                     while let Some((i, j)) = queue.pop_front() {
-                        if visited[i][j] {
+                        if grid[i][j] == 0 {
                             continue;
                         }
                         s += grid[i][j];
-                        visited[i][j] = true;
+                        grid[i][j] = 0;
                         for k in 0..4 {
                             let new_i = i as i32 + DIRECTIONS[k];
                             let new_j = j as i32 + DIRECTIONS[k + 1];
@@ -33,7 +33,7 @@ impl Solution {
                             {
                                 let new_i = new_i as usize;
                                 let new_j = new_j as usize;
-                                if grid[new_i][new_j] > 0 && !visited[new_i][new_j] {
+                                if grid[new_i][new_j] > 0 {
                                     queue.push_back((new_i, new_j));
                                 }
                             }
