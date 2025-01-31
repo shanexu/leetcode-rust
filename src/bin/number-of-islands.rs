6ef301a99d28ad41b4eb34bc15b498b1b0520c70
Shane Xu <xusheng0711@gmail.com>
2025-01-31 20:55:58 +0800

    refine

--- src/bin/number-of-islands.rs
+++ src/bin/number-of-islands.rs
@@ -1,4 +1,14 @@
-fn main() {}
+fn main() {
+    println!(
+        "{}",
+        Solution::num_islands(vec![
+            vec!['1', '1', '1', '1', '0'],
+            vec!['1', '1', '0', '1', '0'],
+            vec!['1', '1', '0', '0', '0'],
+            vec!['0', '0', '0', '0', '0'],
+        ])
+    );
+}
 
 struct Solution;
 
@@ -15,22 +25,24 @@ impl Solution {
                     ans += 1;
                     queue.push_back((i, j));
                     while let Some((row, col)) = queue.pop_front() {
-                        if grid[row][col] == '0' {
-                            continue;
-                        }
-                        grid[row][col] = '0';
-                        for k in 0..4 {
-                            let new_row = row as i32 + DIRECTIONS[k];
-                            let new_col = col as i32 + DIRECTIONS[k + 1];
-                            if new_row >= 0
-                                && new_col >= 0
-                                && new_row < rows as i32
-                                && new_col < cols as i32
-                            {
-                                let new_row = new_row as usize;
-                                let new_col = new_col as usize;
-                                if grid[new_row][new_col] == '1' {
-                                    queue.push_back((new_row, new_col));
+                        if grid[row][col] == '1' {
+                            grid[row][col] = '0';
+                            let mut dx = 0;
+                            let mut dy = 1;
+                            for _ in 0..4 {
+                                let new_row = row as i32 + dx;
+                                let new_col = col as i32 + dy;
+                                (dx, dy) = (dy, -dx);
+                                if new_row >= 0
+                                    && new_col >= 0
+                                    && new_row < rows as i32
+                                    && new_col < cols as i32
+                                {
+                                    let new_row = new_row as usize;
+                                    let new_col = new_col as usize;
+                                    if grid[new_row][new_col] == '1' {
+                                        queue.push_back((new_row, new_col));
+                                    }
                                 }
                             }
                         }
@@ -41,5 +53,3 @@ impl Solution {
         ans
     }
 }
-
-const DIRECTIONS: &[i32] = &[-1, 0, 1, 0, -1];
