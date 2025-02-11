3b30ee04e3a78cc5c4f945410dceea39dd3b1351
Shane Xu <xusheng0711@gmail.com>
2025-02-11 15:12:58 +0800

    add solution

--- src/bin/bricks-falling-when-hit.rs
+++ src/bin/bricks-falling-when-hit.rs
@@ -25,8 +25,8 @@ impl Solution {
         let m = grid.len();
         let n = grid[0].len();
         let mut queue = VecDeque::new();
-        for h in hits.iter() {
-            grid[h[0] as usize][h[1] as usize] = 0;
+        for (i, h) in hits.iter().enumerate() {
+            grid[h[0] as usize][h[1] as usize] = -(i as i32) - 1;
         }
         for i in 0..n {
             if grid[0][i] == 1 {
@@ -53,36 +53,47 @@ impl Solution {
             }
         }
         println!("{:?}", grid);
-        let mut ans = vec![];
-        for h in hits.iter().rev() {
+        let mut ans = vec![0; hits.len()];
+        for t in (0..hits.len()).rev() {
+            let h = &hits[0];
             let i = h[0] as usize;
             let j = h[1] as usize;
-            grid[i][j] = 1;
+            let mut max_hit = grid[i][j];
+            if max_hit >= 0 {
+                continue;
+            }
             let mut c = 0;
             queue.push_back((i, j));
             while let Some((x, y)) = queue.pop_front() {
-                if grid[x][y] == 1 {
-                    c += 1;
-                    grid[x][y] = 2;
-                    let mut dx = 1;
-                    let mut dy = 0;
-                    for _ in 0..4 {
-                        let new_x = x as isize + dx;
-                        let new_y = y as isize + dy;
-                        (dx, dy) = (dy, -dx);
-                        if new_x >= 0 && new_y >= 0 {
-                            let new_x = new_x as usize;
-                            let new_y = new_y as usize;
-                            if new_x < m && new_y < n && grid[new_x][new_y] == 1 {
-                                queue.push_back((new_x, new_y));
-                            }
+                if grid[x][y] == 0 {
+                    continue;
+                }
+                if grid[x][y] == 2 {
+                    continue;
+                }
+                if grid[x][y] < 0 {
+                    max_hit = max_hit.max(grid[x][y]);
+                }
+                c += 1;
+                grid[x][y] = 2;
+                let mut dx = 1;
+                let mut dy = 0;
+                for _ in 0..4 {
+                    let new_x = x as isize + dx;
+                    let new_y = y as isize + dy;
+                    (dx, dy) = (dy, -dx);
+                    if new_x >= 0 && new_y >= 0 {
+                        let new_x = new_x as usize;
+                        let new_y = new_y as usize;
+                        if new_x < m && new_y < n {
+                            queue.push_back((new_x, new_y));
                         }
                     }
                 }
             }
-            ans.push(c - 1);
+            println!("xxx {} {}", c, max_hit);
+            ans[t] = c - 1;
         }
-        ans.reverse();
         ans
     }
 }
