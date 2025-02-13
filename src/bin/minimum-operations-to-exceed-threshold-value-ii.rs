62ed047a6c4dfed08b83a86353cfc426e4779321
Shane Xu <xusheng0711@gmail.com>
2025-02-13 09:26:16 +0800

    refine

--- src/bin/minimum-operations-to-exceed-threshold-value-ii.rs
+++ src/bin/minimum-operations-to-exceed-threshold-value-ii.rs
@@ -12,19 +12,21 @@ use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 impl Solution {
     pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
-        let k = k as i64;
         let mut heap = nums
             .into_iter()
-            .map(|x| Reverse(x as i64))
-            .collect::<BinaryHeap<Reverse<i64>>>();
+            .map(|x| Reverse(x as i32))
+            .collect::<BinaryHeap<Reverse<i32>>>();
         let mut ans = 0;
-        loop {
-            let min = heap.pop().unwrap().0;
+        while let Some(Reverse(min)) = heap.pop() {
             if min >= k {
                 break;
             }
             let max = heap.pop().unwrap().0;
-            heap.push(Reverse(min * 2 + max));
+            heap.push(Reverse(
+                min.checked_mul(2)
+                    .and_then(|x| max.checked_add(x))
+                    .unwrap_or(i32::MAX),
+            ));
             ans += 1;
         }
         ans
