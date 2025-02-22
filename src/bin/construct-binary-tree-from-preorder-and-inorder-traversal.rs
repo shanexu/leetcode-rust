43ea1b71f94f9c6b820f06d56ccbac160b00c895
Shane Xu <xusheng0711@gmail.com>
2025-02-22 10:26:33 +0800

    refine

--- src/bin/construct-binary-tree-from-preorder-and-inorder-traversal.rs
+++ src/bin/construct-binary-tree-from-preorder-and-inorder-traversal.rs
@@ -10,21 +10,35 @@ fn main() {
 struct Solution;
 
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::rc::Rc;
 impl Solution {
     pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        help(&preorder, &inorder)
+        let mut map = HashMap::new();
+        for i in 0..preorder.len() {
+            map.insert(inorder[i], i);
+        }
+        help(&preorder, &inorder, 0, 0, preorder.len(), &map)
     }
 }
 
-fn help(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-    if preorder.len() == 0 {
+fn help(
+    preorder: &[i32],
+    inorder: &[i32],
+    p_pos: usize,
+    i_pos: usize,
+    length: usize,
+    map: &HashMap<i32, usize>,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    if length == 0 {
         return None;
     }
-    let root_val = preorder[0];
+    let root_val = preorder[p_pos];
     let mut root = TreeNode::new(root_val);
-    let i = inorder.iter().position(|&val| val == root_val).unwrap();
-    root.left = help(&preorder[1..i + 1], &inorder[0..i]);
-    root.right = help(&preorder[i + 1..], &inorder[i + 1..]);
-    Some(Rc::new(RefCell::new(root)))
+    let i = *map.get(&root_val).unwrap();
+    let l_len = i - i_pos;
+    let r_len = length - l_len - 1;
+    root.left = help(preorder, inorder, p_pos + 1, i_pos, l_len, map);
+    root.right = help(preorder, inorder, p_pos + 1 + l_len, i + 1, r_len, map);
+    return Some(Rc::new(RefCell::new(root)));
 }
