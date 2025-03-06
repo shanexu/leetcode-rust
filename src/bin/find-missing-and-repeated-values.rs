59e19dc917eec29646662498227db618f1317f19
Shane Xu <xusheng0711@gmail.com>
2025-03-06 08:23:27 +0800

    refine

--- src/bin/find-missing-and-repeated-values.rs
+++ src/bin/find-missing-and-repeated-values.rs
@@ -110,6 +110,7 @@ impl Solution3 {
     pub fn find_missing_and_repeated_values(mut grid: Vec<Vec<i32>>) -> Vec<i32> {
         let n = grid.len();
         let mut ans = vec![0; 2];
+        let mut missing = (1 + n * n) * n * n / 2;
         for i in 0..n {
             for j in 0..n {
                 let v = grid[i][j].abs() as usize;
@@ -120,17 +121,11 @@ impl Solution3 {
                     ans[0] = (x * n + y + 1) as i32
                 } else {
                     grid[x][y] = -u;
+                    missing -= v;
                 }
             }
         }
-        'out: for i in 0..n {
-            for j in 0..n {
-                if grid[i][j] > 0 {
-                    ans[1] = (i * n + j + 1) as i32;
-                    break 'out;
-                }
-            }
-        }
+        ans[1] = missing as i32;
         ans
     }
 }
