42c0393c2f90d1f5dc44f038bfde22fa39b26f7f
Shane Xu <xusheng0711@gmail.com>
2025-03-11 09:48:40 +0800

    add solution

--- src/bin/longest-substring-of-all-vowels-in-order-s2.rs
+++ src/bin/longest-substring-of-all-vowels-in-order-s2.rs
@@ -12,34 +12,21 @@ impl Solution {
     pub fn longest_beautiful_substring(word: String) -> i32 {
         let mut len = 0;
         let mut ans = 0;
-        let mut prev = 0;
+        let mut prev = 93;
         let word = word.as_bytes();
         for &b in word {
-            match (prev, b) {
-                (0, b'a')
-                | (b'a', b'a')
-                | (b'a', b'e')
-                | (b'e', b'e')
-                | (b'e', b'i')
-                | (b'i', b'i')
-                | (b'i', b'o')
-                | (b'o', b'o') => {
-                    prev = b;
-                    len += 1;
-                }
-                (b'o', b'u') | (b'u', b'u') => {
-                    prev = b;
-                    len += 1;
+            if b >= prev && b - prev <= 6 {
+                prev = b;
+                len += 1;
+                if b == b'u' {
                     ans = ans.max(len);
                 }
-                (_, b'a') => {
-                    prev = b'a';
-                    len = 1;
-                }
-                _ => {
-                    prev = 0;
-                    len = 0;
-                }
+            } else if b == b'a' {
+                len = 1;
+                prev = b'a';
+            } else {
+                len = 0;
+                prev = 93;
             }
         }
         ans
