302b6374c446d332bf11871c0cd94c2251a3240d
Shane Xu <xusheng0711@gmail.com>
2025-03-30 09:31:16 +0800

    refine

--- src/bin/partition-labels.rs
+++ src/bin/partition-labels.rs
@@ -7,24 +7,28 @@ fn main() {
 
 struct Solution;
 
-use std::collections::HashSet;
 impl Solution {
     pub fn partition_labels(s: String) -> Vec<i32> {
         let s = s.as_bytes();
         let mut ans = vec![];
         let mut freq = vec![0; 26];
-        let mut letters: HashSet<u8> = HashSet::new();
+        let mut letters = vec![false; 26];
+        let mut letter_count = 0;
         for &b in s {
             freq[(b - b'a') as usize] += 1;
         }
         let mut prev = -1;
         for (i, &b) in s.iter().enumerate() {
-            letters.insert(b);
-            freq[(b - b'a') as usize] -= 1;
-            if freq[(b - b'a') as usize] == 0 {
-                letters.remove(&b);
+            let idx = (b - b'a') as usize;
+            if !letters[idx] {
+                letter_count += 1;
+                letters[idx] = true;
             }
-            if letters.len() == 0 {
+            freq[idx] -= 1;
+            if freq[idx] == 0 {
+                letter_count -= 1;
+            }
+            if letter_count == 0 {
                 ans.push(i as i32 - prev);
                 prev = i as i32;
             }
