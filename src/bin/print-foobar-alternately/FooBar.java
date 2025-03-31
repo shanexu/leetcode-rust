fbb1018172597db989d51f75ded2e8786bf02ebb
Shane Xu <xusheng0711@gmail.com>
2025-03-31 11:23:02 +0800

    add solution

--- src/bin/print-foobar-alternately/FooBar.java
+++ src/bin/print-foobar-alternately/FooBar.java
@@ -1,18 +1,21 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class FooBar {
-    public static void main(String []args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         FooBar fb = new FooBar(3);
         Thread f = new Thread(() -> {
-                try {
-                    fb.foo(() -> System.out.print("foo"));
-                } catch (InternalError | InterruptedException e) {
-                }
+            try {
+                fb.foo(() -> System.out.print("foo"));
+            } catch (InternalError | InterruptedException e) {
+            }
         });
 
         Thread b = new Thread(() -> {
-                try {
-                    fb.bar(() -> System.out.print("bar"));
-                }catch (InternalError | InterruptedException e) {
-                }
+            try {
+                fb.bar(() -> System.out.print("bar"));
+            } catch (InternalError | InterruptedException e) {
+            }
         });
 
         f.start();
@@ -23,28 +26,40 @@ public class FooBar {
     }
 
     private int n;
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition fcond = lock.newCondition();
+    private Condition bcond = lock.newCondition();
+    private boolean foo = true;
 
     public FooBar(int n) {
         this.n = n;
     }
 
-    private java.util.concurrent.atomic.AtomicInteger b = new java.util.concurrent.atomic.AtomicInteger();
-
     public void foo(Runnable printFoo) throws InterruptedException {
+        lock.lock();
         for (int i = 0; i < n; i++) {
-            while (!b.compareAndSet(0, -1));
+            while (!foo) {
+                fcond.await();
+            }
+            foo = !foo;
             // printFoo.run() outputs "foo". Do not change or remove this line.
             printFoo.run();
-            b.set(1);
+            bcond.signal();
         }
+        lock.unlock();
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
+        lock.lock();
         for (int i = 0; i < n; i++) {
-            while (!b.compareAndSet(1, -1));
+            while (foo) {
+                bcond.await();
+            }
+            foo = !foo;
             // printBar.run() outputs "bar". Do not change or remove this line.
             printBar.run();
-            b.set(0);
+            fcond.signal();
         }
+        lock.unlock();
     }
 }
