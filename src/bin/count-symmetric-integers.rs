37ae9f6aa3eff737d54b8d501ebcb344f45842e1
Shane Xu <xusheng0711@gmail.com>
2025-04-12 08:01:49 +0800

    refine

--- src/bin/count-symmetric-integers.rs
+++ src/bin/count-symmetric-integers.rs
@@ -7,22 +7,37 @@ struct Solution;
 impl Solution {
     pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
         let mut ans = 0;
-        for i in low..=high {
-            let s = format!("{}", i);
-            let s = s.as_bytes();
-            let n = s.len();
-            if n % 2 == 0 {
-                let mut l = 0i32;
-                let mut r = 0i32;
-                for j in 0..(n / 2) {
-                    l += s[j] as i32;
-                    r += s[n / 2 + j] as i32;
-                }
-                if l == r {
+        let mut x = low;
+        let mut range_low = 10;
+        let mut range_high = 99;
+        let mut half = 1;
+        while x <= high {
+            if x < range_low {
+                x = range_low
+            } else if x > range_high {
+                range_low *= 100;
+                range_high = range_high * 100 + 99;
+                half += 1;
+            } else {
+                let (r, s1) = sum_digits(x, half);
+                let (_, s2) = sum_digits(r, half);
+                if s1 == s2 {
                     ans += 1;
                 }
+                x += 1;
             }
         }
         ans
     }
+}
+
+#[inline]
+fn sum_digits(mut num: i32, mut n: usize) -> (i32, i32) {
+    let mut s = 0;
+    while n > 0 {
+        s += num % 10;
+        num /= 10;
+        n -= 1;
+    }
+    (num, s)
 }
