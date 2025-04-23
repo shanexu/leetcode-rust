507b946fdc1068fae8ed68bc272fa06acb0944d4
Shane Xu <xusheng0711@gmail.com>
2025-04-24 00:00:55 +0800

    add solution

---
+++ src/bin/count-largest-group.rs
@@ -0,0 +1,40 @@
+
+
+fn main() {
+    println!("{:?}", Solution::count_largest_group(13));
+}
+
+struct Solution;
+
+use std::collections::HashMap;
+impl Solution {
+    pub fn count_largest_group(n: i32) -> i32 {
+        let mut freq = HashMap::new();
+        let mut max = 0;
+        for i in 1..=n {
+            let s = digit_sum(i);
+            let e = freq.entry(s).or_insert(0);
+            *e += 1;
+            if max < *e {
+                max = *e;
+            }
+        }
+        let mut ans = 0;
+        for (_, v) in freq {
+            if v == max {
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
+
+#[inline]
+fn digit_sum(mut n: i32) -> i32 {
+    let mut s = 0;
+    while n > 0 {
+        s += n % 10;
+        n /= 10;
+    }
+    s
+}
